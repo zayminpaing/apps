479f34ac653c724b479086f81ea52103d7e536b0
zayminpaing <32790591+zayminpaing@users.noreply.github.com>
2020-06-11 20:22:02 +0630

    Add files via upload

--- backup.sh
+++ backup.sh
@@ -1,36 +1,30 @@
 #! /bin/bash
 PREFIX="bck_"
 DATE="$PREFIX$(date +%Y%m%d)"
-FILE="$(ls /home/pi/Desktop/ZMP/HDB/PITI_PYAE_ZONE_COMPANY | grep "$DATE")"
+FILE="$(ls /home/pi/Desktop/ZMP/HDB/AGGREGATE_LIVE | grep "$DATE")"
 if [[ "$FILE" == *"$DATE"* ]]; then
-    cp -r /home/pi/Desktop/ZMP/HDB/PITI_PYAE_ZONE_COMPANY/"$FILE" /home/pi/Desktop/ZMP/HDB_HUAWEI/PITI_PYAE_ZONE_COMPANY/
-    echo Success,PITI_PYAE_ZONE_COMPANY,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/PITI_PYAE_ZONE_COMPANY/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
-    echo Success,PITI_PYAE_ZONE_COMPANY,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/PITI_PYAE_ZONE_COMPANY/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_success.txt
+    echo AGGREGATE_LIVE >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    echo -n Start Time =  >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    date +%H%M%S >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    cp -r /home/pi/Desktop/ZMP/HDB/AGGREGATE_LIVE/"$FILE" /home/pi/Desktop/ZMP/HDB_HUAWEI/AGGREGATE_LIVE/
+    echo -n End Time =  >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    date +%H%M%S >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    echo >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    echo Success,AGGREGATE_LIVE,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/AGGREGATE_LIVE/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
+    echo Success,AGGREGATE_LIVE,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/AGGREGATE_LIVE/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_success.txt
 else
-    echo Failed,PITI_PYAE_ZONE_COMPANY,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
-    echo Failed,PITI_PYAE_ZONE_COMPANY,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_failed.txt
-fi
-FILE="$(ls /home/pi/Desktop/ZMP/HDB/UAT_MAI | grep "$DATE")"
-if [[ "$FILE" == *"$DATE"* ]]; then
-    cp -r /home/pi/Desktop/ZMP/HDB/UAT_MAI/"$FILE" /home/pi/Desktop/ZMP/HDB_HUAWEI/UAT_MAI/
-    echo Success,UAT_MAI,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/UAT_MAI/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
-    echo Success,UAT_MAI,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/UAT_MAI/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_success.txt
-else
-    echo Failed,UAT_MAI,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
-    echo Failed,UAT_MAI,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_failed.txt
-fi
-FILE="$(ls /home/pi/Desktop/ZMP/HDB/AGGREGARE_LIVE | grep "$DATE")"
-if [[ "$FILE" == *"$DATE"* ]]; then
-    cp -r /home/pi/Desktop/ZMP/HDB/AGGREGARE_LIVE/"$FILE" /home/pi/Desktop/ZMP/HDB_HUAWEI/AGGREGARE_LIVE/
-    echo Success,AGGREGARE_LIVE,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/AGGREGARE_LIVE/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
-    echo Success,AGGREGARE_LIVE,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/AGGREGARE_LIVE/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_success.txt
-else
-    echo Failed,AGGREGARE_LIVE,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
-    echo Failed,AGGREGARE_LIVE,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_failed.txt
+    echo Failed,AGGREGATE_LIVE,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
+    echo Failed,AGGREGATE_LIVE,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_failed.txt
 fi
 FILE="$(ls /home/pi/Desktop/ZMP/HDB/ASC_LIVE | grep "$DATE")"
 if [[ "$FILE" == *"$DATE"* ]]; then
+    echo ASC_LIVE >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    echo -n Start Time =  >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    date +%H%M%S >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
     cp -r /home/pi/Desktop/ZMP/HDB/ASC_LIVE/"$FILE" /home/pi/Desktop/ZMP/HDB_HUAWEI/ASC_LIVE/
+    echo -n End Time =  >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    date +%H%M%S >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    echo >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
     echo Success,ASC_LIVE,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/ASC_LIVE/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
     echo Success,ASC_LIVE,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/ASC_LIVE/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_success.txt
 else
@@ -39,7 +33,13 @@ else
 fi
 FILE="$(ls /home/pi/Desktop/ZMP/HDB/AWBA_LIVE | grep "$DATE")"
 if [[ "$FILE" == *"$DATE"* ]]; then
+    echo AWBA_LIVE >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    echo -n Start Time =  >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    date +%H%M%S >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
     cp -r /home/pi/Desktop/ZMP/HDB/AWBA_LIVE/"$FILE" /home/pi/Desktop/ZMP/HDB_HUAWEI/AWBA_LIVE/
+    echo -n End Time =  >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    date +%H%M%S >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    echo >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
     echo Success,AWBA_LIVE,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/AWBA_LIVE/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
     echo Success,AWBA_LIVE,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/AWBA_LIVE/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_success.txt
 else
@@ -48,7 +48,13 @@ else
 fi
 FILE="$(ls /home/pi/Desktop/ZMP/HDB/EVO_LIVE | grep "$DATE")"
 if [[ "$FILE" == *"$DATE"* ]]; then
+    echo EVO_LIVE >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    echo -n Start Time =  >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    date +%H%M%S >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
     cp -r /home/pi/Desktop/ZMP/HDB/EVO_LIVE/"$FILE" /home/pi/Desktop/ZMP/HDB_HUAWEI/EVO_LIVE/
+    echo -n End Time =  >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    date +%H%M%S >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    echo >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
     echo Success,EVO_LIVE,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/EVO_LIVE/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
     echo Success,EVO_LIVE,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/EVO_LIVE/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_success.txt
 else
@@ -57,7 +63,13 @@ else
 fi
 FILE="$(ls /home/pi/Desktop/ZMP/HDB/MAI_LIVE | grep "$DATE")"
 if [[ "$FILE" == *"$DATE"* ]]; then
+    echo MAI_LIVE >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    echo -n Start Time =  >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    date +%H%M%S >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
     cp -r /home/pi/Desktop/ZMP/HDB/MAI_LIVE/"$FILE" /home/pi/Desktop/ZMP/HDB_HUAWEI/MAI_LIVE/
+    echo -n End Time =  >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    date +%H%M%S >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    echo >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
     echo Success,MAI_LIVE,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/MAI_LIVE/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
     echo Success,MAI_LIVE,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/MAI_LIVE/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_success.txt
 else
@@ -66,7 +78,13 @@ else
 fi
 FILE="$(ls /home/pi/Desktop/ZMP/HDB/MKTK_LIVE | grep "$DATE")"
 if [[ "$FILE" == *"$DATE"* ]]; then
+    echo MKTK_LIVE >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    echo -n Start Time =  >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    date +%H%M%S >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
     cp -r /home/pi/Desktop/ZMP/HDB/MKTK_LIVE/"$FILE" /home/pi/Desktop/ZMP/HDB_HUAWEI/MKTK_LIVE/
+    echo -n End Time =  >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    date +%H%M%S >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    echo >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
     echo Success,MKTK_LIVE,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/MKTK_LIVE/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
     echo Success,MKTK_LIVE,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/MKTK_LIVE/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_success.txt
 else
@@ -75,7 +93,13 @@ else
 fi
 FILE="$(ls /home/pi/Desktop/ZMP/HDB/MSCN_LIVE | grep "$DATE")"
 if [[ "$FILE" == *"$DATE"* ]]; then
+    echo MSCN_LIVE >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    echo -n Start Time =  >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    date +%H%M%S >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
     cp -r /home/pi/Desktop/ZMP/HDB/MSCN_LIVE/"$FILE" /home/pi/Desktop/ZMP/HDB_HUAWEI/MSCN_LIVE/
+    echo -n End Time =  >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    date +%H%M%S >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    echo >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
     echo Success,MSCN_LIVE,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/MSCN_LIVE/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
     echo Success,MSCN_LIVE,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/MSCN_LIVE/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_success.txt
 else
@@ -84,37 +108,95 @@ else
 fi
 FILE="$(ls /home/pi/Desktop/ZMP/HDB/PAHTAMA | grep "$DATE")"
 if [[ "$FILE" == *"$DATE"* ]]; then
+    echo PAHTAMA >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    echo -n Start Time =  >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    date +%H%M%S >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
     cp -r /home/pi/Desktop/ZMP/HDB/PAHTAMA/"$FILE" /home/pi/Desktop/ZMP/HDB_HUAWEI/PAHTAMA/
+    echo -n End Time =  >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    date +%H%M%S >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    echo >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
     echo Success,PAHTAMA,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/PAHTAMA/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
     echo Success,PAHTAMA,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/PAHTAMA/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_success.txt
 else
     echo Failed,PAHTAMA,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
     echo Failed,PAHTAMA,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_failed.txt
 fi
+FILE="$(ls /home/pi/Desktop/ZMP/HDB/PITI_PYAE_ZONE_COMPANY | grep "$DATE")"
+if [[ "$FILE" == *"$DATE"* ]]; then
+    echo PITI_PYAE_ZONE_COMPANY >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    echo -n Start Time =  >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    date +%H%M%S >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    cp -r /home/pi/Desktop/ZMP/HDB/PITI_PYAE_ZONE_COMPANY/"$FILE" /home/pi/Desktop/ZMP/HDB_HUAWEI/PITI_PYAE_ZONE_COMPANY/
+    echo -n End Time =  >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    date +%H%M%S >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    echo >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    echo Success,PITI_PYAE_ZONE_COMPANY,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/PITI_PYAE_ZONE_COMPANY/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
+    echo Success,PITI_PYAE_ZONE_COMPANY,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/PITI_PYAE_ZONE_COMPANY/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_success.txt
+else
+    echo Failed,PITI_PYAE_ZONE_COMPANY,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
+    echo Failed,PITI_PYAE_ZONE_COMPANY,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_failed.txt
+fi
 FILE="$(ls /home/pi/Desktop/ZMP/HDB/UAT_AWBA | grep "$DATE")"
 if [[ "$FILE" == *"$DATE"* ]]; then
-    cp -r /home/pi/Desktop/ZMP/HDB/UAT_AWBA/"$FILE" /home/pi/Desktop/ZMP/HDB_HUAWEI/UAT_AWBA/
+    echo UAT_AWBA >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    echo -n Start Time =  >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    date +%H%M%S >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    cp -r /home/pi/Desktop/ZMP/HDB/UAT_AWBA/"$FILE" /home/pi/Desktop/ZMP/HDB_HUAWEI/UAT_AWBA
+    echo -n End Time =  >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    date +%H%M%S >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    echo >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
     echo Success,UAT_AWBA,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/UAT_AWBA/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
     echo Success,UAT_AWBA,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/UAT_AWBA/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_success.txt
 else
     echo Failed,UAT_AWBA,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
     echo Failed,UAT_AWBA,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_failed.txt
 fi
-FILE="$(ls /home/pi/Desktop/ZMP/HDB/VILLAGE_LINK_V2 | grep "$DATE")"
+FILE="$(ls /home/pi/Desktop/ZMP/HDB/UAT_MAI | grep "$DATE")"
 if [[ "$FILE" == *"$DATE"* ]]; then
-    cp -r /home/pi/Desktop/ZMP/HDB/VILLAGE_LINK_V2/"$FILE" /home/pi/Desktop/ZMP/HDB_HUAWEI/VILLAGE_LINK_V2/
-    echo Success,VILLAGE_LINK_V2,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/VILLAGE_LINK_V2/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
-    echo Success,VILLAGE_LINK_V2,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/VILLAGE_LINK_V2/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_success.txt
+    echo UAT_MAI >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    echo -n Start Time =  >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    date +%H%M%S >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    cp -r /home/pi/Desktop/ZMP/HDB/UAT_MAI/"$FILE" /home/pi/Desktop/ZMP/HDB_HUAWEI/UAT_MAI/
+    echo -n End Time =  >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    date +%H%M%S >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    echo >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    echo Success,UAT_MAI,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/UAT_MAI/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
+    echo Success,UAT_MAI,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/UAT_MAI/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_success.txt
 else
-    echo Failed,VILLAGE_LINK_V2,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
-    echo Failed,VILLAGE_LINK_V2,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_failed.txt
+    echo Failed,UAT_MAI,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
+    echo Failed,UAT_MAI,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_failed.txt
+fi
+FILE="$(ls /home/pi/Desktop/ZMP/HDB/VILLAGE_LINE_V2 | grep "$DATE")"
+if [[ "$FILE" == *"$DATE"* ]]; then
+    echo VILLAGE_LINE_V2 >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    echo -n Start Time =  >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    date +%H%M%S >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    cp -r /home/pi/Desktop/ZMP/HDB/VILLAGE_LINE_V2/"$FILE" /home/pi/Desktop/ZMP/HDB_HUAWEI/VILLAGE_LINE_V2/
+    echo -n End Time =  >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    date +%H%M%S >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    echo >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    echo Success,VILLAGE_LINE_V2,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/VILLAGE_LINE_V2/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
+    echo Success,VILLAGE_LINE_V2,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/VILLAGE_LINE_V2/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_success.txt
+else
+    echo Failed,VILLAGE_LINE_V2,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
+    echo Failed,VILLAGE_LINE_V2,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_failed.txt
 fi
 FILE="$(ls /home/pi/Desktop/ZMP/HDB/WSR_LIVE_FINAL | grep "$DATE")"
 if [[ "$FILE" == *"$DATE"* ]]; then
+    echo WSR_LIVE_FINAL >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    echo -n Start Time =  >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    date +%H%M%S >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
     cp -r /home/pi/Desktop/ZMP/HDB/WSR_LIVE_FINAL/"$FILE" /home/pi/Desktop/ZMP/HDB_HUAWEI/WSR_LIVE_FINAL/
+    echo -n End Time =  >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    date +%H%M%S >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
+    echo >> /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/duration.txt
     echo Success,WSR_LIVE_FINAL,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/WSR_LIVE_FINAL/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
     echo Success,WSR_LIVE_FINAL,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/WSR_LIVE_FINAL/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_success.txt
 else
     echo Failed,WSR_LIVE_FINAL,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
     echo Failed,WSR_LIVE_FINAL,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_failed.txt
 fi
+mkdir /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)
+cp /home/pi/Desktop/ZMP/HDB/temp_log.txt /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/
+cp /home/pi/Desktop/ZMP/HDB/temp_success.txt /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/
+cp /home/pi/Desktop/ZMP/HDB/temp_failed.txt /home/pi/Desktop/ZMP/HDB_HUAWEI/LOG/$(date +%Y%m%d)/
