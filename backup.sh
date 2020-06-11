a165130c8158d0769596a5f696889194a1cdf4ce
zayminpaing <32790591+zayminpaing@users.noreply.github.com>
2020-06-11 16:19:22 +0630

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -19,3 +19,102 @@ else
     echo Failed,UAT_MAI,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
     echo Failed,UAT_MAI,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_failed.txt
 fi
+FILE="$(ls /home/pi/Desktop/ZMP/HDB/AGGREGARE_LIVE | grep "$DATE")"
+if [[ "$FILE" == *"$DATE"* ]]; then
+    cp -r /home/pi/Desktop/ZMP/HDB/AGGREGARE_LIVE/"$FILE" /home/pi/Desktop/ZMP/HDB_HUAWEI/AGGREGARE_LIVE/
+    echo Success,AGGREGARE_LIVE,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/AGGREGARE_LIVE/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
+    echo Success,AGGREGARE_LIVE,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/AGGREGARE_LIVE/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_success.txt
+else
+    echo Failed,AGGREGARE_LIVE,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
+    echo Failed,AGGREGARE_LIVE,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_failed.txt
+fi
+FILE="$(ls /home/pi/Desktop/ZMP/HDB/ASC_LIVE | grep "$DATE")"
+if [[ "$FILE" == *"$DATE"* ]]; then
+    cp -r /home/pi/Desktop/ZMP/HDB/ASC_LIVE/"$FILE" /home/pi/Desktop/ZMP/HDB_HUAWEI/ASC_LIVE/
+    echo Success,ASC_LIVE,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/ASC_LIVE/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
+    echo Success,ASC_LIVE,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/ASC_LIVE/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_success.txt
+else
+    echo Failed,ASC_LIVE,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
+    echo Failed,ASC_LIVE,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_failed.txt
+fi
+FILE="$(ls /home/pi/Desktop/ZMP/HDB/AWBA_LIVE | grep "$DATE")"
+if [[ "$FILE" == *"$DATE"* ]]; then
+    cp -r /home/pi/Desktop/ZMP/HDB/AWBA_LIVE/"$FILE" /home/pi/Desktop/ZMP/HDB_HUAWEI/AWBA_LIVE/
+    echo Success,AWBA_LIVE,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/AWBA_LIVE/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
+    echo Success,AWBA_LIVE,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/AWBA_LIVE/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_success.txt
+else
+    echo Failed,AWBA_LIVE,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
+    echo Failed,AWBA_LIVE,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_failed.txt
+fi
+FILE="$(ls /home/pi/Desktop/ZMP/HDB/EVO_LIVE | grep "$DATE")"
+if [[ "$FILE" == *"$DATE"* ]]; then
+    cp -r /home/pi/Desktop/ZMP/HDB/EVO_LIVE/"$FILE" /home/pi/Desktop/ZMP/HDB_HUAWEI/EVO_LIVE/
+    echo Success,EVO_LIVE,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/EVO_LIVE/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
+    echo Success,EVO_LIVE,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/EVO_LIVE/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_success.txt
+else
+    echo Failed,EVO_LIVE,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
+    echo Failed,EVO_LIVE,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_failed.txt
+fi
+FILE="$(ls /home/pi/Desktop/ZMP/HDB/MAI_LIVE | grep "$DATE")"
+if [[ "$FILE" == *"$DATE"* ]]; then
+    cp -r /home/pi/Desktop/ZMP/HDB/MAI_LIVE/"$FILE" /home/pi/Desktop/ZMP/HDB_HUAWEI/MAI_LIVE/
+    echo Success,MAI_LIVE,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/MAI_LIVE/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
+    echo Success,MAI_LIVE,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/MAI_LIVE/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_success.txt
+else
+    echo Failed,MAI_LIVE,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
+    echo Failed,MAI_LIVE,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_failed.txt
+fi
+FILE="$(ls /home/pi/Desktop/ZMP/HDB/MKTK_LIVE | grep "$DATE")"
+if [[ "$FILE" == *"$DATE"* ]]; then
+    cp -r /home/pi/Desktop/ZMP/HDB/MKTK_LIVE/"$FILE" /home/pi/Desktop/ZMP/HDB_HUAWEI/MKTK_LIVE/
+    echo Success,MKTK_LIVE,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/MKTK_LIVE/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
+    echo Success,MKTK_LIVE,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/MKTK_LIVE/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_success.txt
+else
+    echo Failed,MKTK_LIVE,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
+    echo Failed,MKTK_LIVE,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_failed.txt
+fi
+FILE="$(ls /home/pi/Desktop/ZMP/HDB/MSCN_LIVE | grep "$DATE")"
+if [[ "$FILE" == *"$DATE"* ]]; then
+    cp -r /home/pi/Desktop/ZMP/HDB/MSCN_LIVE/"$FILE" /home/pi/Desktop/ZMP/HDB_HUAWEI/MSCN_LIVE/
+    echo Success,MSCN_LIVE,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/MSCN_LIVE/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
+    echo Success,MSCN_LIVE,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/MSCN_LIVE/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_success.txt
+else
+    echo Failed,MSCN_LIVE,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
+    echo Failed,MSCN_LIVE,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_failed.txt
+fi
+FILE="$(ls /home/pi/Desktop/ZMP/HDB/PAHTAMA | grep "$DATE")"
+if [[ "$FILE" == *"$DATE"* ]]; then
+    cp -r /home/pi/Desktop/ZMP/HDB/PAHTAMA/"$FILE" /home/pi/Desktop/ZMP/HDB_HUAWEI/PAHTAMA/
+    echo Success,PAHTAMA,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/PAHTAMA/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
+    echo Success,PAHTAMA,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/PAHTAMA/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_success.txt
+else
+    echo Failed,PAHTAMA,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
+    echo Failed,PAHTAMA,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_failed.txt
+fi
+FILE="$(ls /home/pi/Desktop/ZMP/HDB/UAT_AWBA | grep "$DATE")"
+if [[ "$FILE" == *"$DATE"* ]]; then
+    cp -r /home/pi/Desktop/ZMP/HDB/UAT_AWBA/"$FILE" /home/pi/Desktop/ZMP/HDB_HUAWEI/UAT_AWBA/
+    echo Success,UAT_AWBA,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/UAT_AWBA/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
+    echo Success,UAT_AWBA,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/UAT_AWBA/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_success.txt
+else
+    echo Failed,UAT_AWBA,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
+    echo Failed,UAT_AWBA,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_failed.txt
+fi
+FILE="$(ls /home/pi/Desktop/ZMP/HDB/VILLAGE_LINK_V2 | grep "$DATE")"
+if [[ "$FILE" == *"$DATE"* ]]; then
+    cp -r /home/pi/Desktop/ZMP/HDB/VILLAGE_LINK_V2/"$FILE" /home/pi/Desktop/ZMP/HDB_HUAWEI/VILLAGE_LINK_V2/
+    echo Success,VILLAGE_LINK_V2,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/VILLAGE_LINK_V2/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
+    echo Success,VILLAGE_LINK_V2,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/VILLAGE_LINK_V2/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_success.txt
+else
+    echo Failed,VILLAGE_LINK_V2,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
+    echo Failed,VILLAGE_LINK_V2,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_failed.txt
+fi
+FILE="$(ls /home/pi/Desktop/ZMP/HDB/WSR_LIVE_FINAL | grep "$DATE")"
+if [[ "$FILE" == *"$DATE"* ]]; then
+    cp -r /home/pi/Desktop/ZMP/HDB/WSR_LIVE_FINAL/"$FILE" /home/pi/Desktop/ZMP/HDB_HUAWEI/WSR_LIVE_FINAL/
+    echo Success,WSR_LIVE_FINAL,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/WSR_LIVE_FINAL/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
+    echo Success,WSR_LIVE_FINAL,$(date),$(ls -l --block-size=MB /home/pi/Desktop/ZMP/HDB/WSR_LIVE_FINAL/"$FILE" | grep "total") >> /home/pi/Desktop/ZMP/HDB/temp_success.txt
+else
+    echo Failed,WSR_LIVE_FINAL,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_log.txt
+    echo Failed,WSR_LIVE_FINAL,$(date) >> /home/pi/Desktop/ZMP/HDB/temp_failed.txt
+fi
